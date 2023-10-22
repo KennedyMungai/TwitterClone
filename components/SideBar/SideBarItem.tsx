bff8c03ea9c06bbe4ed8c0301a04202644768a70
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-22 14:15:23 +0300

    Properly rendered the sidebar icons

--- components/SideBar/SideBarItem.tsx
+++ components/SideBar/SideBarItem.tsx
@@ -10,10 +10,7 @@ type Props = {
 const SideBarItem = ({ href, label, icon: Icon, onClick }: Props) => {
 	return (
 		<div className='flex flex-row items-center'>
-			<div className='relative rounded-full h-14 items-center justify-center p-4 hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer lg:hidden'>
-				<Icon size={28} color='white' />
-			</div>
-			<div className='relative rounded-full h-14 items-center justify-center p-4 hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer lg:hidden'>
+			<div className='relative rounded-full h-14 items-center justify-center p-10 hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer flex flex-row gap-8'>
 				<Icon size={28} color='white' />
 				<p className='hidden lg:block text-white text-xl'>{label}</p>
 			</div>
