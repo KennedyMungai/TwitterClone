3a5d2b5676d46aa94bc14841f64eacb7b2aca427
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-22 11:53:59 +0300

    Some more stylings for the NavBar

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,7 +1,11 @@
 type Props = {}
 
 const NavBar = (props: Props) => {
-	return <nav className='w-full h-[10vh]'>NavBar</nav>
+	return (
+		<nav className='w-full h-[10vh] border-b-2 border-white dark:border-zinc-800 shadow-md flex justify-between items-center'>
+			NavBar
+		</nav>
+	)
 }
 
 export default NavBar
