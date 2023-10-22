f0c8b861ff55e2bd7f843230a278ef3e0330c1e4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-22 14:34:27 +0300

    Added an FaFeather icon

--- components/SideBar/SideBarTweet.tsx
+++ components/SideBar/SideBarTweet.tsx
@@ -1,9 +1,20 @@
+'use client'
+import { useRouter } from 'next/navigation'
 import React from 'react'
+import { FaFeather } from 'react-icons/fa'
 
 type Props = {}
 
 const SideBarTweet = (props: Props) => {
-	return <div>SideBarTweet</div>
+	const router = useRouter()
+
+	return (
+		<div onClick={() => router.push('/')}>
+			<div className='mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer'>
+				<FaFeather size={24} color={'white'} />
+			</div>
+		</div>
+	)
 }
 
 export default SideBarTweet
