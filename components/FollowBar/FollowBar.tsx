5852d02dd35c76cfd6ccb433c70290470af84c27
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-22 14:58:25 +0300

    Styled the component bar

--- components/FollowBar/FollowBar.tsx
+++ components/FollowBar/FollowBar.tsx
@@ -5,7 +5,14 @@ type Props = {}
 const FollowBar = (props: Props) => {
 	return (
 		<section className='h-full w-[25vw] border-l border-zinc-800 p-6 hidden lg:block'>
-			FollowBar
+			<div className='bg-neutral-800 rounded-xl p-4'>
+				<h2 className='text-white text-xl font-semibold text-center'>
+					Who To Follow
+				</h2>
+				<div className='flex flex-col gap-6 mt-4'>
+					{/* TODO User List*/}
+				</div>
+			</div>
 		</section>
 	)
 }
