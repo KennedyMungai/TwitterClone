3ef187905e0a1d5588a91881097b6b64d297a0f6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-22 11:51:45 +0300

    Rendered out the NavBar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import './globals.css'
+import NavBar from '@/components/NavBar/NavBar'
 
 const open_sans = Open_Sans({ subsets: ['latin'] })
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<body className={open_sans.className}>{children}</body>
+			<body className={open_sans.className}>
+				<NavBar />
+				{children}
+			</body>
 		</html>
 	)
 }
