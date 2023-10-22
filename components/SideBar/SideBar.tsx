90da091fd52c01950535c94f310aef5c53da8f43
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-22 12:11:51 +0300

    Added the Array of objects

--- components/SideBar/SideBar.tsx
+++ components/SideBar/SideBar.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { BsBellFill, BsHouseFill } from 'react-icons/bs'
+import { FaUser } from 'react-icons/fa'
 
 type Props = {}
 
 const SideBar = (props: Props) => {
+	const items = [
+		{
+			label: 'Home',
+			href: '/',
+			icon: <BsHouseFill />
+		},
+		{
+			label: 'Notifications',
+			href: '/notifications',
+			icon: <BsBellFill />
+		},
+		{
+			label: 'Profile',
+			href: '/users/123',
+			icon: <FaUser />
+		}
+	]
+
 	return (
 		<section className='h-full w-[25vw] border-r border-white dark:border-zinc-800'>
 			SideBar
