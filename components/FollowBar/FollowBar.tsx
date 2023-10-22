ad4cfb9a58923e244153ce4eee2d77169ff8e676
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-22 12:03:34 +0300

    Created the FollowBar component

---
+++ components/FollowBar/FollowBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const FollowBar = (props: Props) => {
+	return (
+		<section className='h-full w-[30vw] border-l border-zinc-800'>
+			FollowBar
+		</section>
+	)
+}
+
+export default FollowBar
