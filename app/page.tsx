ebab4aa07e7d3edb2bdbf0b9e96932921f387620
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-22 12:01:20 +0300

    Updated some changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import MainContent from '@/components/MainContent/MainContent'
 import SideBar from '@/components/SideBar/SideBar'
 
 export default function Home() {
 	return (
 		<main className='h-[90vh] w-screen'>
 			<SideBar />
+			<MainContent />
 		</main>
 	)
 }
--- components/MainContent/MainContent.tsx
+++ components/MainContent/MainContent.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 type Props = {}
 
-const MainContext = (props: Props) => {
+const MainContent = (props: Props) => {
 	return (
 		<section className='h-full w-[40vw] bg-zinc-800'>MainContext</section>
 	)
 }
 
-export default MainContext
+export default MainContent
