589cabc65ea7eccdab801bf14ad33470e6016f9f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-22 14:36:55 +0300

    Made a bunch of changes

--- components/SideBar/SideBarTweet.tsx
+++ components/SideBar/SideBarTweet.tsx
@@ -9,7 +9,10 @@ const SideBarTweet = (props: Props) => {
 	const router = useRouter()
 
 	return (
-		<div onClick={() => router.push('/')}>
+		<div
+			className='w-full flex items-center justify-center'
+			onClick={() => router.push('/')}
+		>
 			<div className='mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer'>
 				<FaFeather size={24} color={'white'} />
 			</div>
