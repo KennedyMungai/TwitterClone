0abe65003f8de7878095e0426848ea8f967bad6b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-22 15:47:56 +0300

    Added some more markup for the modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -45,6 +45,10 @@ const Modal = ({
 			<div className='justify-center items-center flex overflow-x-hidden overflow-y-hidden fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800 bg-opacity-70'>
 				<div className='relative w-full my-6 mx-auto h-full lg:w-3/6 lg:max-w-3xl lg:h-auto'>
 					{/* TODO: Add Content */}
+					<div className='h-full border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-black outline-none lg:w-auto focus:outline-none'>
+						{/* TODO: Add Header */}
+						<div className='flex items-center justify-between p-10 rounded-t'></div>
+					</div>
 				</div>
 			</div>
 		</>
