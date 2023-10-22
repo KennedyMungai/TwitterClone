51a6f67abaced668a94db9e0ab0c042079fe27c8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-22 15:04:35 +0300

    Created the handleBack function

--- components/MainContent/Header.tsx
+++ components/MainContent/Header.tsx
@@ -1,6 +1,16 @@
+'use client'
+import { useRouter } from 'next/navigation'
+import { useCallback } from 'react'
+
 type Props = { label: string }
 
 const Header = ({ label }: Props) => {
+	const router = useRouter()
+
+	const handleBack = useCallback(() => {
+		router.back()
+	}, [router])
+
 	return <div>Header</div>
 }
 
