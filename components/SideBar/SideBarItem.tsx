beab738056048aeca6f42dddac47a84f85f06863
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-22 14:01:35 +0300

    Rendered out the icon in the props

--- components/SideBar/SideBarItem.tsx
+++ components/SideBar/SideBarItem.tsx
@@ -7,8 +7,12 @@ type Props = {
 	onClick?: () => void
 }
 
-const SideBarItem = ({ href, label, icon, onClick }: Props) => {
-	return <div>SideBarItem</div>
+const SideBarItem = ({ href, label, icon: Icon, onClick }: Props) => {
+	return (
+		<div className='flex flex-row items-center'>
+				<div className='relative rounded-full h-14 items-center justify-center p-4 hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer lg:hidden'><Icon /></div>
+		</div>
+	)
 }
 
 export default SideBarItem
