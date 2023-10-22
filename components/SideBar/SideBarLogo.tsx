0d4d6539e028b04c42bbad498e3c30fcf80ef496
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-22 13:45:00 +0300

    Added an arrow function for the onClick event handler

--- components/SideBar/SideBarLogo.tsx
+++ components/SideBar/SideBarLogo.tsx
@@ -8,7 +8,7 @@ const SideBarLogo = (props: Props) => {
 	const router = useRouter()
 
 	return (
-		<div className='rounded-full w-20 h-20 p-4 flex items-center justify-center hover:bg-blue-300 hover:bg-opacity-10 cursor-pointer transition' onClick={() =>}>
+		<div className='rounded-full w-20 h-20 p-4 flex items-center justify-center hover:bg-blue-300 hover:bg-opacity-10 cursor-pointer transition' onClick={() => router.push('/')}>
 			<BsTwitter className='text-blue-500 text-5xl' />
 		</div>
 	)
