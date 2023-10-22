fc03072ed6cd46e89db6379ad8e0882512f7bc52
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-22 12:20:56 +0300

    Styled the SideBarLogo component

--- components/SideBar/SideBarLogo.tsx
+++ components/SideBar/SideBarLogo.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+'use client'
+import { useRouter } from 'next/navigation'
+import { BsTwitter } from 'react-icons/bs'
 
 type Props = {}
 
 const SideBarLogo = (props: Props) => {
-  return (
-    <div>SideBarLogo</div>
-  )
+	const router = useRouter()
+
+	return (
+		<div className='rounded-full w-20 h-20 p-4 flex items-center justify-center hover:bg-blue-300 hover:bg-opacity-10 cursor-pointer transition'>
+			<BsTwitter className='text-blue-500 text-5xl' />
+		</div>
+	)
 }
 
 export default SideBarLogo
