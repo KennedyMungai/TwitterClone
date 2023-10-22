1874666e5cffa8a512927bf6fcf3f71786790256
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-22 12:00:33 +0300

    Created the MainContent component

---
+++ components/MainContent/MainContext.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const MainContext = (props: Props) => {
+	return (
+		<section className='h-full w-[40vw] bg-zinc-800'>MainContext</section>
+	)
+}
+
+export default MainContext
